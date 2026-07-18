4e2eb83d0f05c7646f993bd6fc947702978a19c3
blueorbitz <blueorbitz@users.noreply.github.com>
2026-07-18 16:50:05 +0000

    Build: 6c0fbe69237c47345e594719b848a7dab2a78fb4 6c0fbe69237c47345e594719b848a7dab2a78fb4

--- client/assets/swimlanesDiagram-G3AALYLV-C56zqT9s.js
+++
@@ -1,8 +0,0 @@
-import{n as e}from"./chunk-Y2CYZVJY-DsF7k-Jl.js";import"./src-DrO-ttgU.js";import"./chunk-WYO6CB5R-VKKJ0Pdc.js";import"./chunk-ICXQ74PX-DxfVw1WN.js";import"./chunk-HOUHSVGY-CgmKXmGd.js";import"./chunk-Q4XR5HBZ-D7SayWHK.js";import"./chunk-7BUUIJ7U-Bb538aSH.js";import"./chunk-OGEWGWER-DQd-i9X7.js";import"./chunk-32BRIVSS-BUIVC6RH.js";import"./chunk-XXDRQBXY-9MSLhvcq.js";import"./chunk-VR4S4FIN-9NFQl948.js";import"./chunk-C7G6YPKG-Z0KO7Qfu.js";import"./chunk-ZGVPDNZ5-usWaZbNs.js";import"./chunk-52WLFC77-WNonPRix.js";import"./chunk-FWX5IMBZ-ByaAxiwA.js";import"./chunk-ZIRB5QZD-C6fEPe3t.js";import{r as t,t as n}from"./chunk-PUDLZKDR-Dd1EbJwz.js";var r=n({defaultLayout:`swimlane`,styles:e(e=>`${t(e)}
-  .swimlane.cluster rect {
-    stroke: ${e.clusterBorder} !important;
-  }
-  [data-look="neo"].cluster rect {
-    filter: none;
-  }
-`,`getStyles`)});export{r as diagram};
